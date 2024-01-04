d1d71073c00a974c4c0af4b1e53c63c212b647e5
Latorre7 <oscarlatorre7@gmail.com>
2024-01-04 23:10:44 +0100

    Funcion stop

--- main.py
+++ main.py
@@ -2,21 +2,14 @@ from config import *
 import telebot
 from web_scrapping_etsy import etsyP 
 
-
-
 bot = telebot.TeleBot("6803354093:AAH9cdZtNjcNyKIECnGb2SR_Earm97PIyAE")
 
-
-
-
 def guardar_url(user_id, url):
-
     objeto = etsyP(url)
 
 @bot.message_handler(commands=["start"])
 def comandos(message):
-    bot.reply_to(message,"Hola, ¿cómo estas?")
-
+    bot.reply_to(message, "Hola, ¿cómo estas?")
 
 @bot.message_handler(commands=["guardarurl"])
 def comando_guardar_url(message):
@@ -26,21 +19,25 @@ def comando_guardar_url(message):
     # Guardar la URL utilizando la función
     guardar_url(message.from_user.id, url)
 
-     # Imprimir la URL en el terminal
+    # Imprimir la URL en el terminal
     print(f"URL guardada para el usuario {message.from_user.id}: {url}")
 
     bot.reply_to(message, f"URL guardada: {url}")
 
 @bot.message_handler(content_types=["text"])
-def mensaje (message):
+def mensaje(message):
     if message.text.startswith("/"):
         bot.reply_to(message, "Comando no disponible")
     else:
         bot.reply_to(message, "Texto recibido") 
 
-
+@bot.message_handler(commands=["stop"])
+def comando_stop(message):
+    bot.reply_to(message, "Deteniendo el bot...")
+    # Finalizar el bot
+    bot.stop_polling()
 
 if __name__ == "__main__":
     print('Iniciando el Bot')
-    bot.infinity_polling()
+    bot.polling()
     print('Finalizado el Bot')
